c3d40140d6f97e7ada35c3f6b23eba56db77b398
Jamilur Rahman <abir2jamilur30@gmail.com>
2019-01-27 20:40:49 +0600

    v 01

---
+++ contest/src/circle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class circle {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        float x1, x2, x3, y1, y2, y3;
+        x1 = sc.nextFloat();
+        y1 = sc.nextFloat();
+        x2 = sc.nextFloat();
+        y2 = sc.nextFloat();
+        x3 = sc.nextFloat();
+        y3 = sc.nextFloat();
+
+        float
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.show();
+
+        int count =0;
+        Button btn = new Button();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
